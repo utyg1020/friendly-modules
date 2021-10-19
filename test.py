e0383ea16b8f931d11f0a0d115e12e29d98ac53b
utyg1020 <92787261+utyg1020@users.noreply.github.com>
2021-10-19 13:19:09 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from .. import loader
-import random
-async def client_ready(self, client, db):
-        self.db = db 
-
-class testMod(loader.Module):
-    strings = {"name": "Test"}
-    async def testcmd(self, message):
-         await message.respond("Ты гей на " + int(0,100))
