27f3bdcdc91123afeccb7ee9ef21806d6fcb271c
utyg1020 <92787261+utyg1020@users.noreply.github.com>
2021-10-19 13:23:13 +0300

    Update and rename test1.py to test2.py

--- test2.py
+++ test2.py
@@ -5,4 +5,4 @@ class testMod(loader.Module):
     strings = {"name": "Test"}
     async def testcmd(self, message):
          rand = random.randint(0, 100)
-         await message.respond("Ты гей на " + str(rand))
+         await message.respond("Ты гей на " + str(rand) + "%")
