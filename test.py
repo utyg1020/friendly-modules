f48960621152ef64bccecd8a7ef07cb17a5b1e67
utyg1020 <92787261+utyg1020@users.noreply.github.com>
2021-10-19 13:09:42 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from .. import loader
+import random
 async def client_ready(self, client, db):
         self.db = db 
 
 class testMod(loader.Module):
     strings = {"name": "Test"}
     async def testcmd(self, message):
-        await message.respond("Привет это мой модуль!")
+         await message.respond("Ты гей на " + random.randint(0,100))
