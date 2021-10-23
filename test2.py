51fe66632db1c9bd39bf9ae9d9cace317d748e11
utyg1020 <92787261+utyg1020@users.noreply.github.com>
2021-10-23 12:53:08 +0300

    Delete test2.py

--- test2.py
+++
@@ -1,8 +0,0 @@
-from .. import loader
-import random
-
-class testMod(loader.Module):
-    strings = {"name": "Test"}
-    async def testcmd(self, message):
-         rand = random.randint(0, 100)
-         await message.respond("Ты гей на " + str(rand) + "%")
